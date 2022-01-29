74ab7fde5609ae98665ac8567c5d36555c0e2734
Mariela Amanta <mariela20amanta@gmail.com>
2022-01-29 12:13:12 -0500

    GSA-1 Inicio del proyecto

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from "react"; 
+
 import { StyleSheet, Text, View, Image} from "react-native";
 // import { NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
